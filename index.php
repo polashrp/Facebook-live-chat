fbd37c9812c456bb9e991eb6094fdbe0f5620ec8
Habib PHP Programmer <polashrp2640@gmail.com>
2016-10-12 19:43:55 +0600

    index.php
    Facebook Live chat

---
+++ index.php
@@ -0,0 +1,5 @@
+<div class="contact_icon" style="position: fixed; bottom: 20px; right: 20px; font-size: 20px;font-size: 20px; overflow-x: visible !important; visibility: initial; z-index: 1;">
+    <a href="https://m.me/chartingboss" onclick="window.open(this.href,'popUpWindow','right=0,bottom=0,top=10,scrollbars=yes,menubar=no'); return false;">
+         <i class="fa fa-comments" aria-hidden="true"></i> Live Chat
+    </a>
+</div>
